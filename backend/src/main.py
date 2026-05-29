827571dd90cd4cb2d3f91ac72f1d3ad6202924ce
steadydevs-ddmdros <steandydevs@gmail.com>
2026-05-28 21:41:14 -0300

    added debug function to main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,25 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from src.api.chat_routes import router as chat_router
 
 app = FastAPI(title="NetPlay P2P Matcher API", version="1.0.0")
 
-
-origins =[
-    "https://llm-project-tau.vercel.app", 
+origins = [
+    "https://llm-project-tau.vercel.app",
     "http://localhost:5173"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["*"], 
 )
-
-app.include_router(chat_router, prefix="/api")
 
 @app.get("/")
 def read_root():
     return {"message": "NetPlay API is up and running!"}
+
+app.include_router(chat_router, prefix="/api")
+
+
+from fastapi import Request
+
+@app.middleware("http")
+async def debug_cors(request: Request, call_next):
+    origin = request.headers.get("origin")
+    print(f"DEBUG: Requisição vindo de: {origin}")
+    response = await call_next(request)
+    return response
